e0678d781d38d51305e042a741de3f852c87a564
hyunao <89890370+hyuna323@users.noreply.github.com>
2023-06-24 04:57:59 +0900

    loading page

--- js/portfolio2.js
+++ js/portfolio2.js
@@ -60,3 +60,24 @@ $(function(){
 
 // 이력서 다운로드
 document.contect_info = '../download/publisher_ohhyuna.pdf';
+
+// loading page
+const loader = document.querySelector('.loader');
+const html = document.querySelector('html');
+
+html.style.overflow = 'hidden'; //로딩 중 스크롤 방지
+
+window.addEventListener('load', ()=>{
+
+    setTimeout(() => { 
+    
+    loader.style.opacity = '0';
+		html.style.overflow = 'auto'; //스크롤 방지 해제
+
+    setTimeout(() => {
+    loader.style.display = 'none';
+  }, 400);
+        
+  }, 1000);
+
+})
